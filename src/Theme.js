24ad9103dd815af397e921f70f9e44384e160e27
Konrad Pardyak <konradpardyak@gmail.com>
2021-05-10 16:34:55 +0200

    Create Theme

---
+++ src/Theme.js
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    main: "#1A100F",
+    primary: "#FF5638",
+    primaryDark: "#E64D32",
+    secondary: "#FAA419",
+    secondaryDark: "#D98E16",
+    hover: "rgba(0,0,0,.1)",
+    hoverLight: "rgba(255,255,255,.1)"
+  },
+  gradients: {
+    main: "linear-gradient(90deg, #FAA419, #FF5638)",
+    mainDark: "linear-gradient(90deg, #D98E16, #E64D32)"
+  }
+}
+
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;
